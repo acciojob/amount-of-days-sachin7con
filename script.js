20cdc6f69ccd7f960fdea9bcf9c73499be112185
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-02-20 17:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+//SGN your JS code here. If required.
+var noOfDays = function (yr){
+ 	var isleap = 355;
+	var dif = 2024 - yr ;
+	if (dif%4==0) {
+		isleap = 366;
+	} else {
+		isleap = 365;
+	}
+ 
+ 	return (isleap);
+ };
+
+const yr = prompt("Enter year.");
+alert(noOfDays(yr));
